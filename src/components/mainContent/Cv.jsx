ec7d1b0099f1a16ba21b39b812d175a41f3163bf
JordanSerafini <jordanserafini.74@gmail.com>
2023-11-05 11:38:55 +0100

    :construction: work in progress

--- src/components/mainContent/Cv.jsx
+++ src/components/mainContent/Cv.jsx
@@ -1,36 +1,31 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faEnvelope, faHome, faBirthdayCake } from '@fortawesome/free-solid-svg-icons';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
+// Assurez-vous que le chemin d'importation est correct pour le PDF.
+const pdfUrl = '../assets/cv-alternance.pdf';
 
 function Cv() {
   return (
     <div className="Cv">
-      <div className="FirstBlock">
-        <div className="Competence">
-          Competence, portofoglio
-        </div>
-        <div className="Info">
-          <div className="InfoPerso">
-            <h3>informations personnelles</h3>
-            <ul>
-              <li className="infoList">NOM: Jordan SERAFINI - Date de Naissance: 21/01/1990</li>
-              <li className="infoList">
-                <FontAwesomeIcon icon={faHome} /> Adresse: 7 avenue de loverchy, 74000 Annecy
-              </li>
-              <li className="infoList">
-                <FontAwesomeIcon icon={faPhone} /> Téléphone: 0627086475
-              </li>
-              <li className="infoList">
-                <FontAwesomeIcon icon={faEnvelope} /> Email: Jordanserafini.74@gmail.com
-              </li>
-              <li className="infoList">
-                <FontAwesomeIcon icon={faBirthdayCake} /> 
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="SecondBlock"></div>
+      {/* Intégration du PDF dans la page */}
+      <object 
+        data={pdfUrl} 
+        type="application/pdf" 
+        width="100%" 
+        height="600">
+          {/* Message pour les navigateurs qui ne peuvent pas afficher le PDF */}
+          <p>Votre navigateur ne peut pas afficher le PDF. 
+             <a href={pdfUrl}>Cliquez ici pour télécharger le CV.</a>
+          </p>
+      </object>
+
+      {/* Bouton de téléchargement */}
+      <a href={pdfUrl} download="CV-Alternance.pdf">
+        <button className="download-button">
+          <FontAwesomeIcon icon={faDownload} /> Télécharger le CV
+        </button>
+      </a>
     </div>
   );
 }
--- src/components/mainContent/Portofoglio.jsx
+++ src/components/mainContent/Portofoglio.jsx
@@ -1,7 +1,29 @@
 function Portofoglio() {
   return (
     <div className="Portofoglio">
-      <p>Portofoglio</p>
+      <h2>
+      Bienvenue dans mon univers de développement
+      </h2>    
+      <h4>
+        Projet Principal: <span>Immo-Pros</span>
+      </h4>
+      <p>
+        Plongez au cœur de mon projet majeur, une application dédiée au monde de l'immobilier qui illustre mon habileté à conjuguer des solutions back-end robustes avec une expérience utilisateur fluide et intuitive sur le front-end. Technologies utilisées : Node.js, React, PostgreSQL, etc.
+      </p>
+      <a href="https://immopros-app.com/login"> Lien vers le site </a>
+      <h4>
+        Expérimentations et Contributions Open Source
+      </h4>
+      <p>
+        Mes contributions sur GitHub témoignent de mon engagement dans la communauté du code open source. Chaque repository est une opportunité d'apprendre, de partager et de collaborer.
+      </p>
+      <a href="https://github.com/JordanSerafini"> Lien vers mon Github</a>
+      <h4>
+        Compétences Techniques
+      </h4>
+      <p>
+        Ma palette de compétences s'étend sur les technologies les plus récentes et les plus demandées du développement web. Je maîtrise JavaScript et son écosystème, et j'ai une expérience pratique avec des bases de données comme PostgreSQL, des frameworks tels que React, ainsi que des outils de développement moderne comme Docker et Socket.IO.
+      </p>
     </div>
   );
 }
