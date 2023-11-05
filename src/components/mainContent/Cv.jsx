e5fd2317c36541f7f50228bc9380f62a410c6470
JordanSerafini <jordanserafini.74@gmail.com>
2023-11-05 11:44:15 +0100

    :construction: work in progress

--- src/components/mainContent/Cv.jsx
+++ src/components/mainContent/Cv.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
-// Assurez-vous que le chemin d'importation est correct pour le PDF.
-const pdfUrl = '../assets/cv-alternance.pdf';
-
 function Cv() {
+  // Le fichier PDF est référencé directement depuis le dossier public
+  const pdfUrl = '/cv-alternance.pdf';
+
   return (
     <div className="Cv">
-      {/* Intégration du PDF dans la page */}
-      <object 
-        data={pdfUrl} 
-        type="application/pdf" 
-        width="100%" 
-        height="600">
-          {/* Message pour les navigateurs qui ne peuvent pas afficher le PDF */}
-          <p>Votre navigateur ne peut pas afficher le PDF. 
-             <a href={pdfUrl}>Cliquez ici pour télécharger le CV.</a>
-          </p>
-      </object>
+      {/* Intégration du PDF dans la page avec iframe */}
+      <iframe 
+        src={pdfUrl}
+        width="100%"
+        height="600"
+        title="CV"
+        style={{ border: 'none' }}>
+        {/* Message pour les navigateurs qui ne peuvent pas afficher le PDF */}
+        <p>Votre navigateur ne peut pas afficher le PDF. 
+           <a href={pdfUrl} download>Click here to download the CV.</a>
+        </p>
+      </iframe>
 
       {/* Bouton de téléchargement */}
       <a href={pdfUrl} download="CV-Alternance.pdf">
